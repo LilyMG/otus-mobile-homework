d5459b5b8c08628e7f7090415cb5bd3e214b970b
lilittevosyan <lilittevosyan@github.com>
2024-09-26 16:55:02 +0400

    reverted changes to run on an emulator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,58 +1,32 @@
 package org.example;
 
-import com.codeborne.selenide.appium.SelenideAppium;
-import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
-//import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.MobileElement;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
-import static com.codeborne.selenide.Condition.visible;
-import com.codeborne.selenide.Configuration;
-import providers.SelenideWebDriver;
-
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.appium.AppiumClickOptions.tap;
-
-//import static io.appium.java_client.remote.MobileCapabilityType.*;
-//import static org.openqa.selenium.remote.CapabilityType.PLATFORM_NAME;
+import static io.appium.java_client.remote.MobileCapabilityType.*;
+import static org.openqa.selenium.remote.CapabilityType.PLATFORM_NAME;
 
 public class Main {
 
     @Test
-    void testAndroidTap() {
-        Configuration.browser = SelenideWebDriver.class.getName();
-        SelenideAppium.launchApp();
-        $(AppiumBy.xpath(".//*[@text='Views']")).shouldBe(visible).click(tap());
-        $(AppiumBy.xpath(".//*[@text='Animation']")).shouldBe(visible);
-    }
-
-    @Test
     public void test() {
-//        Map<String, Object> selenoidOptions = new HashMap<>();
-//        selenoidOptions.put("browserName", "chrome");
-//        selenoidOptions.put("browserVersion", "100.0");
-//        selenoidOptions.put("enableVNC", true);
-//        selenoidOptions.put("enableVideo", false);
-//        selenoidOptions.put("deviceName", "Samsung Galaxy S6");
-//        selenoidOptions.put("app", "app.apk");
-//        UiAutomator2Options options = new UiAutomator2Options();
-//        options.setCapability("selenoid:options", selenoidOptions);
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability(PLATFORM_NAME, "Android");
+        desiredCapabilities.setCapability(DEVICE_NAME, "emulator-5554");
+        desiredCapabilities.setCapability(APP, "/Users/lilittevosyan/Documents/dev/autoProjects/otus-mobile-no-5/src/main/resources/app-debug.apk");
+        desiredCapabilities.setCapability(AUTOMATION_NAME, "UiAutomator2");
 
-//
-//        try {
-//            new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), options);
-//        } catch (MalformedURLException e) {
-//            throw new RuntimeException(e);
-//        }
+        AppiumDriver<MobileElement> driver;
+        try {
+            driver = new AppiumDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
