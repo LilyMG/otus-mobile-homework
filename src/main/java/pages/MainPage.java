98ae8122ce77ce554d41ea96546aa4b1993edeb6
lilittevosyan <lilittevosyan@github.com>
2024-10-01 22:42:57 +0400

    removed unused imports

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,12 +1,6 @@
 package pages;
 
-import com.codeborne.selenide.Selenide;
-import io.appium.java_client.AppiumDriver;
-
 public class MainPage extends AbsBasePage <MainPage>{
 
-    public MainPage open(){
-        Selenide.open();
-        return this;
-    }
+
 }
