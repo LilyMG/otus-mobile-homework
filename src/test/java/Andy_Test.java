f994706628ab1ae53512445fedb532c1d72c520d
lilittevosyan <lilittevosyan@github.com>
2024-10-06 22:44:56 +0400

    added first test case

--- src/test/java/Andy_Test.java
+++ src/test/java/Andy_Test.java
@@ -2,27 +2,36 @@ import com.google.inject.Inject;
 import extensions.AppiumExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import pages.MainPage;import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import pages.MainPage;
+
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 
 @ExtendWith(AppiumExtension.class)
 
 public class Andy_Test {
 
-
     @Inject
     private MainPage mainPage;
 
+    /**
+     * test case checks visibility of skip button
+     * <p>
+     * open application
+     * check if skip button is invisible
+     * click on next button until it reaches the end (get from helper how any banners user should see)
+     * check if skip button is visible
+     */
     @Test
-    public void testAndroidTap() {
+    public void checkVisibilityOfSkipButton() {
         //TODO here we need a helper to get how many banners are shown to the user to make clicks accordingly
         int amountOfPages = 3;
         mainPage.open();
-        for (int i = 0; i < amountOfPages -1 ; i++) {
+        mainPage.isSkipVisible(false);
+        for (int i = 0; i < amountOfPages - 1; i++) {
             mainPage.clickOnNext();
         }
         mainPage.isSkipVisible(true);
-        System.out.println(mainPage.toString());
     }
 
 }
